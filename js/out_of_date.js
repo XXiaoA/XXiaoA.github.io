2f9dc14f115cf22b49acd9bfe681b1c1455e2612
XXiaoA <isxxiaoa@gmail.com>
2023-01-18 08:03:59 +0000

    add github action yaml file of hexo in article 使用GitHub action自动部署博客

--- js/out_of_date.js
+++ js/out_of_date.js
@@ -11,12 +11,12 @@
   if (interval > 3600*24*30*6*1000 && interval <= 3600*24*30*12*1000){
     var days = parseInt(interval / 86400000)
     posts[0].innerHTML = '<div class="note note-info" style="font-size:0.9rem"><p>' +
-      '<div class="h6">Article Timeliness Reminder</div><p>This article has been ' + days + ' days since the last update, the content of the article may be outdated.' +
+      '<div class="h6">Article Timeliness Reminder</div><p>This article has been ' + days + ' days since the first publication, the content of the article may be outdated.' +
       '</p></p></div>' + posts[0].innerHTML;
   }else if (interval > 3600*24*30*12*1000){
     var days = parseInt(interval / 86400000)
     posts[0].innerHTML = '<div class="note note-warning" style="font-size:0.9rem"><p>' +
-      '<div class="h6">Article Timeliness Reminder</div><p>This article has been ' + days + ' days since the last update, the content of the article may be outdated.' +
+      '<div class="h6">Article Timeliness Reminder</div><p>This article has been ' + days + ' days since the first publication, the content of the article may be outdated.' +
       '</p></p></div>' + posts[0].innerHTML;
   }
 })();
